36b01c43af34e6d60976961a52b1e071e6604f93
knechtel <maiquelknechtel@gmail.com>
2022-02-17 23:15:31 -0300

    Adicionando view que retorna todos os clients.

--- cadastro/views.py
+++ cadastro/views.py
@@ -1,5 +1,10 @@
+import json
+from django.core import serializers
+from cadastro.models import Client
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    clients = Client.objects.all()
+    clients_list = serializers.serialize('json', clients)
+    return HttpResponse(clients_list, content_type="text/json-comment-filtered")
