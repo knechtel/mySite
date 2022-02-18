4315b541951fb7f29aa234c2c47e3e3570b7b5ba
knechtel <maiquelknechtel@gmail.com>
2022-02-18 01:05:48 -0300

    Adicionando pagina root.

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 urlpatterns = [
-    path('cadastro/',  include('cadastro.urls')),
+    path('', include('cadastro.urls')),
+    path('cadastro/', include('cadastro.urls')),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 
